596261d391880e9310f7371359b7b858b14b3e10
Yuki Soma <somay.work@gmail.com>
2021-01-28 14:53:04 +0900

    install local web-encoding and use it

--- RNProject/App.js
+++ RNProject/App.js
@@ -23,8 +23,13 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {TextEncoder, TextDecoder} from 'web-encoding';
 
 const App: () => React$Node = () => {
+  const encoded = new TextEncoder().encode('test');
+  const decoded = new TextDecoder().decode(
+    new Uint8Array([116, 101, 115, 116]),
+  );
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -32,7 +37,6 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
@@ -40,31 +44,16 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+                new TextEncoder().encode('test') =
               </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>{encoded}</Text>
               <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
+                new TextDecoder().decode( new Uint8Array([116, 101, 115, 116]))
+                =
               </Text>
+              <Text style={styles.sectionDescription}>{decoded}</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
